bd766e183251ce0e7538cb2946c2a1e7274d0c9a
pfdzm <pfdzm@users.noreply.github.com>
2019-10-06 13:14:53 +0200

    refine game start, pick random answer

--- javascript/game.js
+++ javascript/game.js
@@ -34,60 +34,83 @@ var attemptCharsId = document.querySelector("#attemptChars");
 
 var warningId = document.querySelector("#warning");
 
+var startGame = true;
+
 document.onkeydown = keyDown;
 
 // fun begins when a user presses a key
 function keyDown(event) {
   var key = event.key;
 
-  // first check if there are any attempts remaining
-  if (attemptCounter > 0) {
-    if (allowedChars.includes(key)) {
-      warningId.textContent = "";
-      if (!attemptChars.includes(key)) {
-        // user had not tried key -> add key to list of attempts, decrease remaining attempts by 1
-        attemptChars += key;
-        --attemptCounter;
-        if (answer.includes(key)) {
-          // user has guessed a correct letter
-          // get the index of the first occurrence of the letter
-          var position = answer.indexOf(key);
-          // get all occurrences of the letter to replace dashes on screen
-          while (position > -1) {
-            // replace underscore with correct letter
-            answerDisplay = replaceDash(answerDisplay, position, key);
-            position = answer.indexOf(key, position + 1);
-          }
-          if (answerDisplay === answer) {
-            warningId.textContent = "You win!";
-          }
-        }
-      }
-    } else {
-      warningId.textContent = "Please select a letter from the latin alphabet.";
-    }
+  if (startGame) {
+    // keep things easy by making everything lowercase
+    answer = pickAnswer(dictionary).toLowerCase();
+    answerDisplay = anonimizeAnswer(answer);
     answerDisplayId.textContent = answerDisplay;
     attemptCharsId.innerHTML =
       `Attempts left: ` +
       attemptCounter +
       `<br/>Attempted letters: ` +
       attemptChars;
-    // } while (answerDisplay.indexOf("_") > -1);
-
-    // playArea.textContent += `${event.key}`;
+    startGame = false;
   } else {
-    // Create a new paragraph element, and append it to the end of the document body
-    warningId.textContent = "Game over!";
+    // first check if there are any attempts remaining
+    if (attemptCounter > 0) {
+      if (allowedChars.includes(key)) {
+        warningId.textContent = "";
+        if (!attemptChars.includes(key)) {
+          // user had not tried key -> add key to list of attempts, decrease remaining attempts by 1
+          attemptChars += key;
+          if (answer.includes(key)) {
+            // user has guessed a correct letter
+            // get the index of the first occurrence of the letter
+            var position = answer.indexOf(key);
+            // get all occurrences of the letter to replace dashes on screen
+            while (position > -1) {
+              // replace underscore with correct letter
+              answerDisplay = replaceDash(answerDisplay, position, key);
+              position = answer.indexOf(key, position + 1);
+            }
+            if (answerDisplay === answer) {
+              warningId.textContent = "You win!";
+            }
+          } else --attemptCounter;
+        }
+      } else {
+        warningId.textContent =
+          "Please select a letter from the latin alphabet.";
+      }
+      answerDisplayId.textContent = answerDisplay;
+      attemptCharsId.innerHTML =
+        `Attempts left: ` +
+        attemptCounter +
+        `<br/>Attempted letters: ` +
+        attemptChars;
+    } else {
+      warningId.textContent = "Game over!";
+    }
   }
 }
 
-// initialize the game with the user's first keypress
+// Custom function to clean up code; takes a string, a position and a substring to replace at said position
 
-// check if the user has not tried this key before
-// do {
-
-// Define a custom function to replace underscores in the 'answerDisplay' string with the actual correct
-// letters
 function replaceDash(strRef, position, str) {
   return strRef.substring(0, position) + str + strRef.substring(position + 1);
+}
+
+// Another custom function to clean up code, this one takes a string a replaces every
+// letter with an underscore
+
+function anonimizeAnswer(str) {
+  var tempStr = "";
+  for (let index = 0; index < str.length; index++) {
+    tempStr += `_`;
+  }
+
+  return tempStr;
+}
+
+function pickAnswer(dictionary) {
+  var randInt = Math.floor(Math.random() * dictionary.length);
+  return dictionary[randInt];
 }
